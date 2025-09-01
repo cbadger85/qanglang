309ec602824beb2a4ca71354e56ed91ab13a3c06
Charles Badger <charles.badger@gmail.com>
2025-09-01 18:56:14 -0500

    finish typed view of node sea

--- crates/qanglang-core/src/frontend/typed_node_arena.rs
+++ crates/qanglang-core/src/frontend/typed_node_arena.rs
@@ -381,6 +381,18 @@ impl TryFrom<AstNode> for ForInitializerNode {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+pub struct TypedNodeRef<T> {
+    pub id: NodeId,
+    pub node: T,
+}
+
+impl<T> TypedNodeRef<T> {
+    pub fn new(id: NodeId, node: T) -> Self {
+        Self { id, node }
+    }
+}
+
 pub struct TypedNodeArena {
     nodes: Arena<AstNode>,
     node_array: NodeArrayArena,
@@ -402,121 +414,149 @@ impl TypedNodeArena {
         &self.nodes[node_id.get()]
     }
 
-    pub fn get_decl_node(&self, node_id: NodeId) -> DeclNode {
+    pub fn get_decl_node(&self, node_id: NodeId) -> TypedNodeRef<DeclNode> {
         let node = self.nodes[node_id.get()];
-        node.try_into().unwrap()
+        TypedNodeRef::new(node_id, node.try_into().unwrap())
     }
 
-    pub fn get_stmt_node(&self, node_id: NodeId) -> StmtNode {
+    pub fn get_stmt_node(&self, node_id: NodeId) -> TypedNodeRef<StmtNode> {
         let node = self.nodes[node_id.get()];
-        node.try_into().unwrap()
+        TypedNodeRef::new(node_id, node.try_into().unwrap())
     }
 
-    pub fn get_expr_node(&self, node_id: NodeId) -> ExprNode {
+    pub fn get_expr_node(&self, node_id: NodeId) -> TypedNodeRef<ExprNode> {
         let node = self.nodes[node_id.get()];
-        node.try_into().unwrap()
+        TypedNodeRef::new(node_id, node.try_into().unwrap())
     }
 
-    pub fn get_call_operation_node(&self, node_id: NodeId) -> CallOperationNode {
+    pub fn get_call_operation_node(&self, node_id: NodeId) -> TypedNodeRef<CallOperationNode> {
         let node = self.nodes[node_id.get()];
-        node.try_into().unwrap()
+        TypedNodeRef::new(node_id, node.try_into().unwrap())
     }
 
-    pub fn get_assignment_target_node(&self, node_id: NodeId) -> AssignmentTargetNode {
+    pub fn get_assignment_target_node(
+        &self,
+        node_id: NodeId,
+    ) -> TypedNodeRef<AssignmentTargetNode> {
         let node = self.nodes[node_id.get()];
-        node.try_into().unwrap()
+        TypedNodeRef::new(node_id, node.try_into().unwrap())
     }
 
-    pub fn get_primary_node(&self, node_id: NodeId) -> PrimaryNode {
+    pub fn get_primary_node(&self, node_id: NodeId) -> TypedNodeRef<PrimaryNode> {
         let node = self.nodes[node_id.get()];
-        node.try_into().unwrap()
+        TypedNodeRef::new(node_id, node.try_into().unwrap())
     }
 
-    pub fn get_var_decl_node(&self, node_id: NodeId) -> VariableDeclNode {
+    pub fn get_var_decl_node(&self, node_id: NodeId) -> TypedNodeRef<VariableDeclNode> {
         let node = self.nodes[node_id.get()];
 
         match node {
-            AstNode::VariableDecl(decl) => decl,
+            AstNode::VariableDecl(decl) => TypedNodeRef::new(node_id, decl),
             _ => panic!("Expected VariableDeclNode."),
         }
     }
 
-    pub fn get_program_node(&self, node_id: NodeId) -> ProgramNode {
+    pub fn get_program_node(&self, node_id: NodeId) -> TypedNodeRef<ProgramNode> {
         let node = self.nodes[node_id.get()];
 
         match node {
-            AstNode::Program(program) => program,
+            AstNode::Program(program) => TypedNodeRef::new(node_id, program),
             _ => panic!("Expected ProgramNode"),
         }
     }
 
-    pub fn get_identifier_node(&self, node_id: NodeId) -> IdentifierNode {
+    pub fn get_identifier_node(&self, node_id: NodeId) -> TypedNodeRef<IdentifierNode> {
         let node = self.nodes[node_id.get()];
         match node {
-            AstNode::Identifier(identifier) => identifier,
+            AstNode::Identifier(identifier) => TypedNodeRef::new(node_id, identifier),
             _ => panic!("Expected IdentifierNode"),
         }
     }
 
-    pub fn get_func_expr_node(&self, node_id: NodeId) -> FunctionExprNode {
+    pub fn get_func_expr_node(&self, node_id: NodeId) -> TypedNodeRef<FunctionExprNode> {
         let node = self.nodes[node_id.get()];
 
         match node {
-            AstNode::FunctionExpr(func) => func,
+            AstNode::FunctionExpr(func) => TypedNodeRef::new(node_id, func),
             _ => panic!("Expected FunctionExprNode"),
         }
     }
 
-    pub fn get_lambda_expr_node(&self, node_id: NodeId) -> LambdaExprNode {
+    pub fn get_lambda_expr_node(&self, node_id: NodeId) -> TypedNodeRef<LambdaExprNode> {
         let node = self.nodes[node_id.get()];
 
         match node {
-            AstNode::LambdaExpr(lambda) => lambda,
+            AstNode::LambdaExpr(lambda) => TypedNodeRef::new(node_id, lambda),
             _ => panic!("Expected LambdaExprNode"),
         }
     }
 
-    pub fn get_block_stmt_node(&self, node_id: NodeId) -> BlockStmtNode {
+    pub fn get_block_stmt_node(&self, node_id: NodeId) -> TypedNodeRef<BlockStmtNode> {
         let node = self.nodes[node_id.get()];
 
         match node {
-            AstNode::BlockStmt(block) => block,
+            AstNode::BlockStmt(block) => TypedNodeRef::new(node_id, block),
             _ => panic!("Expected BlockStmtNode."),
         }
     }
 
-    pub fn get_lambda_body_node(&self, node_id: NodeId) -> LambdaBodyNode {
+    pub fn get_lambda_body_node(&self, node_id: NodeId) -> TypedNodeRef<LambdaBodyNode> {
         let node = self.nodes[node_id.get()];
-        node.try_into().unwrap()
+        TypedNodeRef::new(node_id, node.try_into().unwrap())
     }
 
-    pub fn get_for_initializer_node(&self, node_id: NodeId) -> ForInitializerNode {
+    pub fn get_for_initializer_node(&self, node_id: NodeId) -> TypedNodeRef<ForInitializerNode> {
         let node = self.nodes[node_id.get()];
-        node.try_into().unwrap()
+        TypedNodeRef::new(node_id, node.try_into().unwrap())
     }
 
-    pub fn iter_decl_nodes(array_id: NodeArrayId) {
-        // TODO iterate over the array's node ids, supplying the ones that are declarations. Assume the parser correctly added the right nodes.
-        todo!()
+    pub fn iter_decl_nodes(
+        &self,
+        array_id: NodeArrayId,
+    ) -> impl Iterator<Item = TypedNodeRef<DeclNode>> + '_ {
+        self.node_array
+            .iter(array_id)
+            .map(|node_id| self.get_decl_node(node_id))
     }
 
-    pub fn iter_expr_nodes(array_id: NodeArrayId) {
-        // TODO iterate over the array's node ids, supplying the ones that are expressions. Assume the parser correctly added the right nodes.
-        todo!()
+    pub fn iter_expr_nodes(
+        &self,
+        array_id: NodeArrayId,
+    ) -> impl Iterator<Item = TypedNodeRef<ExprNode>> + '_ {
+        self.node_array
+            .iter(array_id)
+            .map(|node_id| self.get_expr_node(node_id))
     }
 
-    pub fn iter_object_entry_nodes(array_id: NodeArrayId) {
-        // TODO iterate over the array's node ids, supplying the ones that are object entries. Assume the parser correctly added the right nodes.
-        todo!()
+    pub fn iter_object_entry_nodes(
+        &self,
+        array_id: NodeArrayId,
+    ) -> impl Iterator<Item = TypedNodeRef<ObjectEntryNode>> + '_ {
+        self.node_array.iter(array_id).map(|node_id| {
+            let node = self.nodes[node_id.get()];
+            match node {
+                AstNode::ObjectEntry(entry) => TypedNodeRef::new(node_id, entry),
+                _ => panic!("Expected ObjectEntryNode"),
+            }
+        })
     }
 
-    pub fn iter_class_member_nodes(array_id: NodeArrayId) {
-        // TODO iterate over the array's node ids, supplying the ones that are class members. Assume the parser correctly added the right nodes.
-        todo!()
+    pub fn iter_class_member_nodes(
+        &self,
+        array_id: NodeArrayId,
+    ) -> impl Iterator<Item = TypedNodeRef<ClassMember>> + '_ {
+        self.node_array.iter(array_id).map(|node_id| {
+            let node = self.nodes[node_id.get()];
+            TypedNodeRef::new(node_id, ClassMember::try_from(node).unwrap())
+        })
     }
 
-    pub fn iter_identifier_nodes(array_id: NodeArrayId) {
-        // TODO iterate over the array's node ids, supplying the ones that are identifiers. Assume the parser correctly added the right nodes.
-        todo!()
+    pub fn iter_identifier_nodes(
+        &self,
+        array_id: NodeArrayId,
+    ) -> impl Iterator<Item = TypedNodeRef<IdentifierNode>> + '_ {
+        self.node_array
+            .iter(array_id)
+            .map(|node_id| self.get_identifier_node(node_id))
     }
 }
